d315f189587c72ffddf7a1f16660bdaeb222fa40
jayanlee <jayanlee93@gmail.com>
2024-03-21 00:50:18 +0530

    webpack plugin for dynamic chuck inclusion

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin({ template: "index.html" })],
 };
