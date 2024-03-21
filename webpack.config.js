40780bcbe4ebb413352b8e7c2506cb7062c86997
jayanlee <jayanlee93@gmail.com>
2024-03-22 01:40:31 +0530

    adding loader for css, scss and process rules for images and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,21 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
   plugins: [new HtmlWebpackPlugin({ template: "index.html" })],
 };
